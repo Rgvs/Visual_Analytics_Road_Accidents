0536c99adfb57af26ffb64b54d88b4d600022915
rohitcmohan <rohitcmohan@gmail.com>
2017-05-11 01:12:48 -0400

    row charts and us map

--- static/js/parallel.js
+++ static/js/parallel.js
@@ -18,7 +18,7 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("../FARS3_sample.csv", function(error, cars) {
+d3.csv("FARS3_sample.csv", function(error, cars) {
 
   // Extract the list of dimensions and create a scale for each.
   x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
@@ -122,4 +122,3 @@ function brush() {
     }) ? null : "none";
   });
 }
-
